7caffb1d18649288dde0493d7dcc5110df1a5230
ABarsen <kiplagatemmanuel21@gmail.com>
2026-03-05 11:37:08 +0300

    data handling

---
+++ File.py
@@ -0,0 +1,43 @@
+# # read
+
+# f = open(r"d:\codes\Python\File handling\name.txt", "r")
+# # print(f.read())
+
+# for line in f:
+#     print(line)
+
+# f.close()
+
+
+# apend
+# f=open(r"d:\codes\Python\File handling\name.txt","a")
+
+# f.write("neil")
+# f.close()
+
+
+# f=open(r"d:\codes\Python\File handling\name.txt","r")
+
+# print(f.read())
+# f.close()
+
+# files overwrite
+
+# f =open(r"d:\codes\Python\File handling\context.txt","w")
+
+# f.write( "I will delete this content")
+# f.close()
+
+# f =open(r"d:\codes\Python\File handling\context.txt","r")
+
+# print(f.read())
+# f.close()
+
+
+
+# ways of creating a file
+
+# 1. method
+f =open("name_list.txt","w")
+f.close()
+
